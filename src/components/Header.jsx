ff9afe4374971e52c67811617d3709141b0df918
anikatasnin <jarintasnin09@gmail.com>
2025-07-03 20:16:35 +0600

    apply image on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,19 @@
 import headerBanner from "../assets/images/more/15.jpg";
+import logo1 from "../assets/images/more/logo1.png";
 import "../App.css";
 const Header = () => {
   return (
     <div>
       <h2
-        className="text-white text-4xl py-4 text-center bg-cover bg-center title"
+        className="text-white text-4xl py-4 text-center bg-cover bg-center"
         style={{ backgroundImage: `url(${headerBanner})` }}
       >
-        Espresso Emporium
+        <div className="flex items-center justify-center gap-2">
+          <img src={logo1} alt="Logo" className="h-12 w-12" />
+          <span className="font-[Rancho] text-white text-4xl">
+            Espresso Emporium
+          </span>
+        </div>
       </h2>
     </div>
   );
