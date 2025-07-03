a08936a9adf7f95c620ee093119475cb33d3ca90
anikatasnin <jarintasnin09@gmail.com>
2025-07-03 20:11:53 +0600

    apply style on header text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import headerBanner from "../assets/images/more/15.jpg";
+import "../App.css";
 const Header = () => {
   return (
     <div>
       <h2
-        className="text-white text-4xl font-bold py-4 text-center bg-cover bg-center"
+        className="text-white text-4xl py-4 text-center bg-cover bg-center title"
         style={{ backgroundImage: `url(${headerBanner})` }}
       >
         Espresso Emporium
